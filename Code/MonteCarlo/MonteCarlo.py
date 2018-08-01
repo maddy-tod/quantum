0ed58caa7164df9f9b145f348b5065c1a63fc921
maddytod <maddytod@gmail.com>
2018-08-01 16:28:52 +0100

    Refactored to make it easier to extend in future

--- Code/MonteCarlo/MonteCarlo.py
+++ Code/MonteCarlo/MonteCarlo.py
@@ -21,84 +21,84 @@ sys.path.append(os.path.abspath('../QUtils'))
 from qutils import pprint
 
 
-# Create a Quantum Register with 4 qubit.
+# Create a Quantum Register with base size
 q = QuantumRegister(10)
-# Create a Classical Register with 4 bits.
+# Create a Classical Register with base size
 c = ClassicalRegister(10)
 # Create a Quantum Circuit
 qc = QuantumCircuit(q, c)
 
 
 
-def prepareCircuit():
-    # the 1st 4 qubits into an equal superposition
-    # they are the ones which are used to store the inputs
-    qc.h(q[0])
-    qc.h(q[1])
-    qc.h(q[2])
-    qc.h(q[3])
-    qc.h(q[4])
-    qc.h(q[5])
+def prepareCircuit(size):
 
-    adder()
+    global q, c, qc
+    # Create a Quantum Register with base size
+    q = QuantumRegister((size * 3) + 1)
+    # Create a Classical Register with base size
+    c = ClassicalRegister((size * 3) + 1)
+    # Create a Quantum Circuit
+    qc = QuantumCircuit(q, c)
+
+
+
+
+    # the 1st 2 size qubits into an equal superposition
+    # they are the ones which are used to store the inputs
+    for i in range (2*size):
+        qc.h(q[i])
+
+    adder(size)
 
     qc.measure(q,c)
 
-def adder():
+def adder(size):
+    # size is the number of qubits per number to be added
+    # must be > = 2
 
-    """   qc.ccx(q[1], q[3], q[6])
-    qc.cx(q[3], q[5])
-    qc.cx(q[1], q[5])
-    qc.cx(q[0], q[4])
-    qc.cx(q[2], q[4])
+    carry = 3 * size
+    a = 0
+    b = size
+    sum = 2 * size
 
-    qc.cx(q[6], q[4])
-    """
     # deal with the 1st digit
     # set the carry
-    qc.ccx(q[2], q[5], q[9])
+    qc.ccx(q[a+2], q[b+2], q[carry])
     # add to the sum
-    qc.cx(q[2], q[8])
-    qc.cx(q[5], q[8])
+    qc.cx(q[a+2], q[sum+2])
+    qc.cx(q[b+2], q[sum+2])
 
     # add the carry to the next sum digit
-    qc.cx(q[9], q[7])
+    qc.cx(q[carry], q[sum+1])
     # reset the carry
-    qc.ccx(q[2], q[5], q[9])
+    qc.ccx(q[a+2], q[b+2], q[carry])
 
     # get carry for new digits
-    qc.ccx(q[1], q[4], q[9])
-    qc.ccx(q[7], q[4], q[9])
-    qc.ccx(q[1], q[7], q[9])
+    qc.ccx(q[a+1], q[b+1], q[carry])
+    qc.ccx(q[sum+1], q[b+1], q[carry])
+    qc.ccx(q[a+1], q[sum+1], q[carry])
 
     # add the new digits
-    qc.cx(q[1], q[7])
-    qc.cx(q[4], q[7])
+    qc.cx(q[a+1], q[sum+1])
+    qc.cx(q[b+1], q[sum+1])
 
     # add the carry to the next sum digit
-    qc.cx(q[9], q[6])
-    # reset the carry -  not working
-    qc.ccx(q[1], q[4], q[9])
-    qc.x(q[7])
-    qc.ccx(q[4], q[7], q[9])
-    qc.ccx(q[1], q[7], q[9])
-    qc.x(q[7])
-
-    #qc.ccx(q[1], q[4], q[9])
-    #qc.ccx(q[7], q[4], q[9])
-    #qc.ccx(q[1], q[7], q[9])
-
+    qc.cx(q[carry], q[sum])
+    # reset the carry
+    qc.ccx(q[a+1], q[b+1], q[carry])
+    qc.x(q[sum+1])
+    qc.ccx(q[b+1], q[sum+1], q[carry])
+    qc.ccx(q[a+1], q[sum+1], q[carry])
+    qc.x(q[sum+1])
 
     # set the next carry
-    qc.ccx(q[0], q[3], q[9])
-    qc.ccx(q[6], q[3], q[9])
-    qc.ccx(q[0], q[6], q[9])
+    qc.ccx(q[a], q[b], q[carry])
+    qc.ccx(q[sum], q[b], q[carry])
+    qc.ccx(q[a], q[sum], q[carry])
 
     # add the new digits
-    qc.cx(q[0], q[6])
-    qc.cx(q[3], q[6])
-
-
+    qc.cx(q[a], q[sum])
+    qc.cx(q[b], q[sum])
 
 
 def run():
@@ -126,7 +126,11 @@ def run():
         #print(str(res[0:2]) + " + " + str(res[2:4]) + " = " + str(res[4:6]) + " + " + str(res[6]))
 
 def main():
-    prepareCircuit()
+    size = input("How many qubits would you like to add?")
+    while not size.isdecimal():
+        size = input("How many qubits would you like to add?")
+
+    prepareCircuit(int(size))
     run()
 
 main()
