3d75389521d4f43306a9a3cbab722d8bed9dbe7c
maddytod <maddytod@gmail.com>
2018-08-01 15:54:36 +0100

    Added in the ability to add 3 qubit numbers. Going to extend to x bit numbers

--- Code/MonteCarlo/MonteCarlo.py
+++ Code/MonteCarlo/MonteCarlo.py
@@ -22,9 +22,9 @@ from qutils import pprint
 
 
 # Create a Quantum Register with 4 qubit.
-q = QuantumRegister(7)
+q = QuantumRegister(10)
 # Create a Classical Register with 4 bits.
-c = ClassicalRegister(7)
+c = ClassicalRegister(10)
 # Create a Quantum Circuit
 qc = QuantumCircuit(q, c)
 
@@ -37,8 +37,8 @@ def prepareCircuit():
     qc.h(q[1])
     qc.h(q[2])
     qc.h(q[3])
-    #qc.h(q[4])
-    #qc.h(q[5])
+    qc.h(q[4])
+    qc.h(q[5])
 
     adder()
 
@@ -56,25 +56,47 @@ def adder():
     """
     # deal with the 1st digit
     # set the carry
-    qc.ccx(q[1], q[3], q[6])
+    qc.ccx(q[2], q[5], q[9])
     # add to the sum
-    qc.cx(q[1], q[5])
-    qc.cx(q[3], q[5])
-
+    qc.cx(q[2], q[8])
+    qc.cx(q[5], q[8])
 
     # add the carry to the next sum digit
-    qc.cx(q[6], q[4])
+    qc.cx(q[9], q[7])
     # reset the carry
-    qc.ccx(q[1], q[3], q[6])
-    # get carry for new digits
-    qc.ccx(q[0], q[2], q[6])
-    qc.ccx(q[2], q[4], q[6])
-    qc.ccx(q[0], q[4], q[6])
+    qc.ccx(q[2], q[5], q[9])
 
+    # get carry for new digits
+    qc.ccx(q[1], q[4], q[9])
+    qc.ccx(q[7], q[4], q[9])
+    qc.ccx(q[1], q[7], q[9])
 
     # add the new digits
-    qc.cx(q[0], q[4])
-    qc.cx(q[2], q[4])
+    qc.cx(q[1], q[7])
+    qc.cx(q[4], q[7])
+
+    # add the carry to the next sum digit
+    qc.cx(q[9], q[6])
+    # reset the carry -  not working
+    qc.ccx(q[1], q[4], q[9])
+    qc.x(q[7])
+    qc.ccx(q[4], q[7], q[9])
+    qc.ccx(q[1], q[7], q[9])
+    qc.x(q[7])
+
+    #qc.ccx(q[1], q[4], q[9])
+    #qc.ccx(q[7], q[4], q[9])
+    #qc.ccx(q[1], q[7], q[9])
+
+
+    # set the next carry
+    qc.ccx(q[0], q[3], q[9])
+    qc.ccx(q[6], q[3], q[9])
+    qc.ccx(q[0], q[6], q[9])
+
+    # add the new digits
+    qc.cx(q[0], q[6])
+    qc.cx(q[3], q[6])
 
 
 
@@ -90,11 +112,16 @@ def run():
 
     print()
     for res, c in sim_result.get_counts(qc).items() :
+        # reverse the result
         res = res[::-1]
-        a = int(res[0:2], 2)
-        b = int(res[2:4], 2)
-        ans = int(res[4:6], 2)
-        carry =  int(res[6], 2) * 4
+
+        # extract results into variables
+        a = int(res[0:3], 2)
+        b = int(res[3:6], 2)
+        ans = int(res[6:9], 2)
+        carry =  int(res[9], 2) * 8
+
+        # display results
         print(str(a) + " + " + str(b) + " = " + str(ans) + " + " + str(carry))
         #print(str(res[0:2]) + " + " + str(res[2:4]) + " = " + str(res[4:6]) + " + " + str(res[6]))
 
